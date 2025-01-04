2905057da88cfcdf27bfda64ae3dd84559024c28
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-04 20:50:39 +0100

    fix get app error

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in umt/__init__.py
-from umt import __version__ as version
-
 setup(
-    name="umt",  # Keep the package name simple
-    version=version,
+    name="umt",
+    version="1.0.0",
     description="UNEM Management Tool",
     author="UMT",
     author_email="admin@unem.ma",
-    packages=find_packages(),
+    packages=find_packages(where="."),
+    package_dir={"": "."},
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires
