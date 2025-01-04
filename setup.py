586dcb24b326bef76e15d70d7d4968b50ad743d6
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-04 19:36:48 +0100

    fix not founf setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
+# get version from __version__ variable in umt/__init__.py
+from umt import __version__ as version
+
 setup(
     name="umt",
-    version="1.0.0",
-    description="UNEM Management System",
-    author="UNEM",
+    version=version,
+    description="UNEM Management Tool",
+    author="UMT",
     author_email="admin@unem.ma",
     packages=find_packages(),
     zip_safe=False,
