26edcb50cb00701e566bd1358885dd88a46055be
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-05 21:26:49 +0100

    fix the stucture

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in umt/__init__.py
+from umt import __version__ as version
+
 setup(
-    name='umt',
-    version='1.0.0',
-    description='UNEM Management Tool',
-    author='UMT',
-    author_email='admin@unem.ma',
+    name="umt",
+    version=version,
+    description="UNEM Management Tool",
+    author="UMT",
+    author_email="admin@unem.ma",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=[
-        'frappe>=14.0.0',
-        'python-dateutil',
-        'babel',
-        'num2words'
-    ]
+    install_requires=install_requires
 )
